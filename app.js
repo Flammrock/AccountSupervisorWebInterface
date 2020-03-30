7ecf36481dd3872b8436e3f88005e037012b55fe
Flammrock <flammrockgame@gmail.com>
2020-03-30 10:38:04 +0200

    update

--- app.js
+++ app.js
@@ -600,6 +600,21 @@ app.get('/api/discord/login', (req, res) => {
   res.redirect(`https://discordapp.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=identify&response_type=code&redirect_uri=${redirect}`);
 });
 
+app.get('/api/discord/callback', (req, res) => {
+  if (!req.query.code) throw new Error('NoCodeProvided');
+  const code = req.query.code;
+  const creds = btoa(`${CLIENT_ID}:${CLIENT_SECRET}`);
+  const response = await fetch(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirect}`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${creds}`,
+      },
+    });
+  const json = await response.json();
+  res.redirect(`/?token=${json.access_token}`);
+});
+
 app.get('/', function (req, res) {
 	res.status(200).sendFile(path.join(__dirname, 'index.html'));
 });
